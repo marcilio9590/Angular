b25eb38be0d277a5919e76293b0dbf9dd8bf379c
Marcilio Cobel <marcilio.c.junior@accenture.com>
2019-03-10 16:39:03 -0300

    crud de todos no local storage

--- angular-professional/todo/src/app/app.component.ts
+++ angular-professional/todo/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { load } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root', //<app-root>
@@ -6,5 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Funcionou';
+  // tipagem de variavel -> : 
+  // any = qualquer coisa(ex: Object) 
+  // any[] -> uma lista
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.load();
+  }
+
+  remover(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+      this.save();
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+    this.save();
+  }
+
+  add() {
+    const formJson = this.form.value;
+    // this.form.controls['title'].value
+    const title = formJson.title;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(title, false, id));
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    }
+    this.todos
+  }
 }
