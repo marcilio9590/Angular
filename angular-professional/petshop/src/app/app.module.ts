e8042f682d91ff68f2f35ef76c3b7fafa7379ecf
Marcilio Cobel <marcilio.c.junior@accenture.com>
2019-03-24 13:29:25 -0300

    organizando arquivos

--- angular-professional/petshop/src/app/app.module.ts
+++ angular-professional/petshop/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { PetsPageComponent } from './pets-page/pets-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/accounts/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/accounts/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/accounts/signup-page/signup-page.component';
+import { PetsPageComponent } from './pages/accounts/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
