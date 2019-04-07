755a5c4699067d0dd8a603034b3b218acb1354bb
Marcilio Cobel <marcilio.c.junior@accenture.com>
2019-04-06 22:12:39 -0300

    removendo app do petshopt

--- angular-professional/petshop/src/app/pages/master/frame.page.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'frame-page-componente',
-    template: '<app-navbar></app-navbar> <router-outlet></router-outlet>'
-})
-export class FramePageComponent { }
